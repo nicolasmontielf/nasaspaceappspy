b83b80684e23522cb4419529ed9ea64beb1f2f41
nicolasmontielf <56762247+nicolasmontielf@users.noreply.github.com>
2019-10-20 12:34:25 -0300

    Modificacion de las rutas

---
+++ planets.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php 
+    $page="Conoce los destinos";
+    include 'includes/head.php'; 
+    ?>
+
+</head>
+<body style="background-color:black;">
+    <?php include 'menu.php'; ?>
+
+        <div class="planets-page">
+            <section>
+                <div class="container py-3">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="assets/img/planet-pluto.png" width="330px">
+                            </div>
+
+                            <div class="col-md-8 px-3">
+                                <div class="card-block px-3">
+                                    <h4 class="card-title">Pluto</h4>
+                                    <p class="card-text">Pluto is a complex and mysterious world with mountains, valleys, plains, craters, and maybe glaciers. Discovered in 1930, Pluto was long considered our solar system's ninth planet. But after the discovery of similar intriguing worlds deeper in the distant Kuiper Belt, icy Pluto was reclassified as a dwarf planet.</p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container py-3">
+                    <div class="card">
+                        <div class="row ">
+                            <div class="col-md-4">
+                            <img src="assets/img/planet-makemake.png" width="330px">
+                            </div>
+
+                            <div class="col-md-8 px-3">
+                                <div class="card-block px-3">
+                                    <h4 class="card-title">Makemake</h4>
+                                    <p class="card-text">Along with fellow dwarf planets Pluto, Eris and Haumea, Makemake is located in the Kuiper Belt, a region outside the orbit of Neptune. Slightly smaller than Pluto, Makemake is the second-brightest object in the Kuiper Belt as seen from Earth (while Pluto is the brightest). It takes about 305 Earth years for this dwarf planet to make one trip around the sun.</p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section >
+                <div class="container py-3">
+                    <div class="card">
+                        <div class="row ">
+                            <div class="col-md-4">
+                                <img src="assets/img/planet-huamea.png" width="330px">
+                            </div>
+
+                            <div class="col-md-8 px-3">
+                                <div class="card-block px-3">
+                                    <h4 class="card-title">Haumea</h4>
+                                    <p class="card-text">Originally designated 2003 EL61 (and nicknamed Santa by one discovery team), Haumea resides in the Kuiper belt and is roughly the same size as Pluto. Haumea is one of the fastest rotating large objects in our solar system. Its fast spin distorts Haumea's shape, making this dwarf planet look like a football.</p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section >
+                <div class="container py-3">
+                    <div class="card">
+                        <div class="row ">
+                            <div class="col-md-4">
+                                <img src="assets/img/planet-eris.png" width="330px">
+                            </div>
+
+                            <div class="col-md-8 px-3">
+                                <div class="card-block px-3">
+                                    <h4 class="card-title">Eris</h4>
+                                    <p class="card-text">Eris is one of the largest known dwarf planets in our solar system. It's about the same size as Pluto, but is three times farther from the Sun. </p>
+
+                                    <p>Eris first appeared to be larger than Pluto. This triggered a debate in the scientific community that led to the International Astronomical Union's decision in 2006 to clarify the definition of a planet. Pluto, Eris and other similar objects are now classified as dwarf planets.</p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section >
+                <div class="container py-3">
+                    <div class="card">
+                        <div class="row ">
+                            <div class="col-md-4">
+                                <img src="assets/img/planet-ceres.png" width="330px">
+                            </div>
+
+                            <div class="col-md-8 px-3">
+                                <div class="card-block px-3">
+                                    <h4 class="card-title">Ceres</h4>
+                                    <p class="card-text">Dwarf planet Ceres is the largest object in the asteroid belt between Mars and Jupiter and the only dwarf planet located in the inner solar system. It was the first member of the asteroid belt to be discovered when Giuseppe Piazzi spotted it in 1801. And when Dawn arrived in 2015, Ceres became the first dwarf planet to receive a visit from a spacecraft.</p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        <div>
+
+        <?php include 'scripts.php'; ?>
+</body>
+</html>
